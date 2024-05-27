20579e993e33e01b25edbdf2c2b57aa7f28ae239
Xamz <bugi@gmail.com>
2024-05-28 00:41:10 +0300

    one commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat bean2Cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat == bean2Cat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import java.util.Objects;
+
+public class Cat {
+    private String message;
+
+    public Cat(String message){
+    this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
